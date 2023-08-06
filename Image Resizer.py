f3eef2bc38687cf10567c64d9532441d1371a129
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-08-06 17:23:25 +0200

    Update Image Resizer.py

--- Image Resizer.py
+++ Image Resizer.py
@@ -4,6 +4,15 @@ import os
 def resize_and_convert_image(input_path, output_path):
     with Image.open(input_path) as image:
         image.thumbnail((512, 512))
+        
+        if image.size != (512, 512):
+            left_margin = (image.width - 512) / 2
+            top_margin = (image.height - 512) / 2
+            right_margin = (image.width + 512) / 2
+            bottom_margin = (image.height + 512) / 2
+
+            image = image.crop((left_margin, top_margin, right_margin, bottom_margin))
+
         image.save(output_path, "PNG")
 
 source_folder = input("Enter path with all your images that you want to rezise:\n")
@@ -15,8 +24,8 @@ if not os.path.exists(target_folder):
 for file_name in os.listdir(source_folder):
     if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
         source_path = os.path.join(source_folder, file_name)
-        base_name = os.path.splitext(file_name)[0]  
+        base_name = os.path.splitext(file_name)[0]
         target_file_name = base_name + ".png"
         target_path = os.path.join(target_folder, target_file_name)
         resize_and_convert_image(source_path, target_path)
-        print(f"Resized and converted {file_name} to {target_file_name}")
+        print(f"Processed {file_name} to {target_file_name}")
