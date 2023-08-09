28efc8888d36fe3868cbd359f8867ebd82abb9df
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-08-09 18:25:07 +0200

    Update Image Resizer.py

--- Image Resizer.py
+++ Image Resizer.py
@@ -25,7 +25,7 @@ for file_name in os.listdir(source_folder):
     if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
         source_path = os.path.join(source_folder, file_name)
         base_name = os.path.splitext(file_name)[0]
-        target_file_name = base_name + ".png"
+        target_file_name = base_name + " /Users/jonasb./Desktop/testedited.png"
         target_path = os.path.join(target_folder, target_file_name)
         resize_and_convert_image(source_path, target_path)
         print(f"Processed {file_name} to {target_file_name}")
