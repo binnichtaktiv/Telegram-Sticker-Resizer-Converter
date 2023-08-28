8a90fe95d517addda1c99941a7d7544ce86d6418
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-08-28 11:36:57 +0200

    changed output

--- Image Resizer.py
+++ Image Resizer.py
@@ -15,17 +15,18 @@ def resize_and_convert_image(input_path, output_path):
 
         image.save(output_path, "PNG")
 
-source_folder = input("Enter folder path with all your images that you want to rezise:\n")
+source_folder = input("Enter folder path with all your images that you want to resize:\n")
 target_folder = input("Enter output path\n")
 
-if not os.path.exists(target_folder):
-    os.makedirs(target_folder)
+edited_folder_path = os.path.join(target_folder, 'edited')
+if not os.path.exists(edited_folder_path):
+    os.makedirs(edited_folder_path)
 
 for file_name in os.listdir(source_folder):
     if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff')):
         source_path = os.path.join(source_folder, file_name)
         base_name = os.path.splitext(file_name)[0]
         target_file_name = base_name + " edited.png"
-        target_path = os.path.join(target_folder, target_file_name)
+        target_path = os.path.join(edited_folder_path, target_file_name)
         resize_and_convert_image(source_path, target_path)
         print(f"Processed {file_name} to {target_file_name}")
